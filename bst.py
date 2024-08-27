e571af8a4ab9be59bf293127245176e17828fcea
scube <mail.shivanksharma@gmail.com>
2024-08-27 17:36:37 +0530

    bst

--- bst.py
+++ bst.py
@@ -1,4 +1,5 @@
-# BST - Binary Search Tree have atmost two child nodes
+# BST - Binary Search Tree have atmost two child nodes ---- Left side values are smaller and right side values are  greater than that of parent node --- Elements are unique
 
-# Left side values are smaller and right side values are  greater than that of parent node
+# Inorder - Root node is in middle, Postorder - Root node is at the end, Preorder - Root node is at the beginning, Level order Traversal - BFS
 
+ 
